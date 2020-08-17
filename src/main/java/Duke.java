4e85f905f69d2486c6ffc01d2d5cd84842c902f2
Jane-Ng <ngshulinjane@hotmail.com>
2020-08-17 14:32:55 +0800

    Add Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
+    public static void printHorizontalLines() {
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printHorizontalLines();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printHorizontalLines();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLines();
     }
 }
