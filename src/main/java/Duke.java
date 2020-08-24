45fc0142fc392da6b79af5a0d96534a454682cbd
Jane <ngshulinjane@hotmail.com>
2020-08-25 00:31:05 +0800

    Add Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void printHorizontalLines() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
         printHorizontalLines();
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
         printHorizontalLines();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println();
+
+        String command;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            command = in.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                printHorizontalLines();
+                System.out.println("     " + command);
+                printHorizontalLines();
+                System.out.println();
+            }
+        }
+
+        printHorizontalLines();
+        System.out.println("     Bye. Hope to see you again soon!");
         printHorizontalLines();
     }
 }
