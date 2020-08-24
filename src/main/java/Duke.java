6a34e14e6a160afeb893ac0a17d86daf8ee18617
Jane <ngshulinjane@hotmail.com>
2020-08-25 01:00:17 +0800

    Add Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,15 +12,26 @@ public class Duke {
         printHorizontalLines();
         System.out.println();
 
+        String[] tasks = new String[100];
+        int numOfTasks = 0;
         String command;
         Scanner in = new Scanner(System.in);
         while (true) {
             command = in.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                printHorizontalLines();
+                for (int i = 0; i < numOfTasks; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                printHorizontalLines();
+                System.out.println();
             } else {
                 printHorizontalLines();
-                System.out.println("     " + command);
+                tasks[numOfTasks] = command;
+                numOfTasks++;
+                System.out.println("     added: " + command);
                 printHorizontalLines();
                 System.out.println();
             }
