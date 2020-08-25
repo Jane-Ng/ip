5909e016867893cc2da76ac3145af6fcc6235440
Jane <ngshulinjane@hotmail.com>
2020-08-25 11:02:44 +0800

    Add the ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,17 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
     }
 
+    public static void listTasks(Task tasks[], int tasksCount) {
+        if (tasksCount == 0) {
+            System.out.println("     There are no tasks in your list.");
+        } else {
+            System.out.println("     Here are the tasks in your list:");
+            for (int i = 0; i < tasksCount; i++) {
+                System.out.println("     " + (i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         printHorizontalLines();
         System.out.println("     Hello! I'm Duke");
@@ -12,29 +23,31 @@ public class Duke {
         printHorizontalLines();
         System.out.println();
 
-        String[] tasks = new String[100];
-        int numOfTasks = 0;
+        Task[] tasks = new Task[100];
+        int tasksCount = 0;
         String command;
         Scanner in = new Scanner(System.in);
         while (true) {
             command = in.nextLine();
-            if (command.equals("bye")) {
+            if (command.equalsIgnoreCase("bye")) {
                 break;
-            } else if (command.equals("list")) {
-                printHorizontalLines();
-                for (int i = 0; i < numOfTasks; i++) {
-                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
-                }
-                printHorizontalLines();
-                System.out.println();
-            } else {
-                printHorizontalLines();
-                tasks[numOfTasks] = command;
-                numOfTasks++;
-                System.out.println("     added: " + command);
-                printHorizontalLines();
-                System.out.println();
             }
+            printHorizontalLines();
+            if (command.equalsIgnoreCase("list")) {
+                listTasks(tasks, tasksCount);
+            } else if (command.contains("done")) {
+                int separatorPosition = command.indexOf(' ');
+                int taskDoneIndex = Integer.parseInt(command.substring(separatorPosition + 1)) - 1;
+                tasks[taskDoneIndex].markAsDone();
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       [" + tasks[taskDoneIndex].getStatusIcon() + "] " + tasks[taskDoneIndex].getDescription());
+            } else {
+                tasks[tasksCount] = new Task(command);
+                tasksCount++;
+                System.out.println("     added: " + command);
+            }
+            printHorizontalLines();
+            System.out.println();
         }
 
         printHorizontalLines();
